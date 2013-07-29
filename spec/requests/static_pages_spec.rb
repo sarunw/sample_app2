b5dba16fc52aa6436422e38976e7b2e0d17b1bc6
sarunw <sarunw@Arts-MacBook-Air.local>
2013-07-29 19:36:20 +0700

    Make a basic User model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,8 +42,8 @@ describe "Static pages" do
 		click_link "Contact"
 		page.should have_selector 'title', text: full_title('Contact')
 		click_link "Home"
-		# click_link "Sign up now!"
-		# page.should have_selector 'title', text: full_title('Sign up')
+		click_link "Sign up now!"
+		page.should have_selector 'title', text: full_title('Sign up')
 		click_link "sample app"
 		page.should have_selector 'h1', text: 'Sample App'
 	end
