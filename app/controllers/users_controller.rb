e06a0a84d9e924a38c0b6f79c398b121e7b361d8
sarunw <sarunw@Arts-MacBook-Air.local>
2013-07-31 00:30:17 +0700

    Finish updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
 	def show
-		@user = User.find(params[:id])  	
+		@user = User.find(params[:id])
   end
 
   def new
@@ -16,5 +24,47 @@ class UsersController < ApplicationController
   	else
   		render 'new'
   	end
+  end
+
+  def edit
+    # @user defined in correct_user instead
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'  
+    end
+    
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+  end
+
+private
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in"  
+    end
+    
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
+      
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
   end
 end
