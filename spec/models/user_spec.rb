f4cc2ae222b4f66c9375ea359dc11a4283b219de
sarunw <sarunw@Arts-MacBook-Air.local>
2013-07-30 17:56:32 +0700

    finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ describe User do
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:authenticate) }
+	it { should respond_to(:remember_token)}
 
 	it { should be_valid }
 
@@ -103,5 +104,8 @@ describe User do
 
 
 	end
-
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
+	end
 end
